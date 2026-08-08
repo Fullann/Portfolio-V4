156f60eeea58f46beea65ac9fc8546c9e57f242e
Nathan Füllemann <nathan@fullann.ch>
2026-08-08 16:42:33 +0200

    update email

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,7 +1,9 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: process.env.EMAIL_HOST || 'mail.fullann.ch',
+  port: process.env.EMAIL_PORT || 465,
+  secure: true, // true pour le port 465, false pour 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
